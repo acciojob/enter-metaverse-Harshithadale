c2fd71e29c53b420c7ceae967770dd5c7b6e3072
Harshithadale <daleharshi6045@gmail.com>
2025-04-01 12:35:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const status = document.getElementById("status");
+const enterBtn = document.getElementById("enterBtn");
+enterBtn.onclick = ()=>{
+	status.innerHTML = `<h1>Entered Metaverse</h1>`
+}
